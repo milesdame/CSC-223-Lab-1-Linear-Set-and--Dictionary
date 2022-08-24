89c4e29d82492885d89dbc7f22f5da33fc094be1
Sophie-Ngo <sophiegngo@gmail.com>
2022-08-24 16:20:16 -0400

    Update ArraySet.java
    impllemented some methods

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -95,92 +95,83 @@ public class ArraySet<E> implements List<E>, Set<E>
 	
 	
 	@Override
+	// removes all items in collection c from _list
+	// returns true if _list changed at all
 	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		return _list.removeAll(c);
 	}
 
-
 	@Override
+	// removes all items NOT in collection c from _list
+	// returns true if _list changed at all
 	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		return _list.retainAll(c);
 	}
 
-
 	@Override
+	// removes all elements from the list
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		_list.clear();
 	}
 
-
 	@Override
+	// gets the element at the specified index
 	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.get(index);
 	}
 
-
 	@Override
+	// replaces the element at the index. returns the old element (before replacement)
 	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.set(index, element);
 	}
 
-
 	@Override
+	// inserts the element at the index (all elements to the right are shifted right by one)
 	public void add(int index, E element) {
-		// TODO Auto-generated method stub
-		
+		_list.add(index, element);
 	}
 
-
 	@Override
+	// removes the element at the index (all elements to the right are shifted left by one)
 	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.remove(index);
 	}
 
-
 	@Override
+	// returns the index of the first occurence of object o in _list, else -1
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return _list.indexOf(o);
 	}
 
-
 	@Override
+	// returns the index of the last occurence of object o in _list, else -1
 	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return _list.lastIndexOf(o);
 	}
 
-
 	@Override
+	// returns a list iterator over the elements in this list
 	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.listIterator();
 	}
 
-
 	@Override
+	// returns a list iterator over the elements in this list starting at given index
 	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.listIterator(index);
 	}
 
-
 	@Override
+	// returns a list starting from the element at fromIndex and ending with the element at the index before toIndex
 	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		return _list.subList(fromIndex, toIndex);
 	}
 
 	@Override
+	// returns a spliterator over elements in this list
 	public Spliterator<E> spliterator() {
-		// TODO Auto-generated method stub
-		return List.super.spliterator();
+		return _list.spliterator();
 	}
 
 }
